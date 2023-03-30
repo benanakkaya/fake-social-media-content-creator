b3373cb46b32e3617a58953188374a2cea10dd45
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-03-30 22:57:07 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './input.css'
+import './input.css';
+import './output.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
